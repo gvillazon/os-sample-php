1151508f32de59e670dd12b25e186b09d88310e7
Gina Villazon <ginav@Ginas-iMac.local>
2019-02-27 19:56:48 -0500

    change

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php
-  phpinfo();
-?>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>';
+ $servername = "127.0.0.1";
+ $username = "gvillazon";
+ $password = "interval";
+ $dbname = "db";
+
+// Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+ if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+ }
+ echo "Connected successfully<br><br>";
+ $sql = "SELECT PersonID, LastName, FirstName FROM Persons";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "PersonID: " . $row["PersonID"]. " - LastName: " . $row["LastName"]. " " . $row["FirstName"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+ ?>
+ </body>
+</html>
+
